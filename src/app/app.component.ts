c37b9d36aab74a0b16a68d004e5647333626bedd
Azlouk@DESKTOP-QMAGF8A <azlouknour@gmail.com>
2021-11-23 12:20:53 +0100

    qr code coöponents are fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teammembercards';
+
+  data = [{
+    'Nachname' : 'Azlouk',
+    'Vorname' : 'Mohamed Yassine',
+    'Mitglieder-Nr' : '015897254',
+    'MitgliederDatum' : '12.11.2020'
+  }]
+  dataToString = JSON.stringify(this.data);
+  public text=" Guten Tag Ihre Mitglieder, Hier sind Ihre Persönliche Daten bie uns /n Name:Azlouk /n Nachname:Yassin";
+  public qrCodeVal :string ="";
+  public level: "L" | "M" | "Q" | "H" ;
+  public width: number;
+
+   constructor() {
+     this.level="L" ;
+     this.width =200;
+     this.qrCodeVal = "QR Code string value" ;
+   }
+
+   updateLevel(newLevel:"L" | "M" | "Q" | "H"){
+     this.level=newLevel;
+   }
+   updateQrInfo(newValue:string) {
+     this.qrCodeVal=newValue;
+   }
+   updateWidth(newWidth:number) {
+     this.width=newWidth;
+   }
 }
